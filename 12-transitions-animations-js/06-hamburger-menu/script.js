d37f3ffbe3dadac75bf42df810e15b497a2615ca
RustamHajizadaOriginal <rustamhajizada@aol.com>
2025-01-08 12:32:21 +0100

    Updating JS

--- 12-transitions-animations-js/06-hamburger-menu/script.js
+++ 12-transitions-animations-js/06-hamburger-menu/script.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () { 
-    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
-    
-    toggleButton.addEventListener('click', function () {
-        mobileMenu.classList.toggle('active');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  //ToggleButton selector
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
 });
